d7dc7545a419b36d05801e5aee327a4169376291
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-08 16:20:45 +0200

    clase Usuario

--- src/main/java/com/spring/boot/springbootform/controllers/IndexController.java
+++ src/main/java/com/spring/boot/springbootform/controllers/IndexController.java
@@ -3,13 +3,33 @@ package com.spring.boot.springbootform.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.boot.springbootform.models.Usuario;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({ "/index", "/", "/home" })
+    @GetMapping({ "/index", "/", "", "/home" })
     public String index(Model model) {
         model.addAttribute("titulo", "Hola  Spring Framework");
         return "index";
     }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Rodrigo");
+        usuario.setApellido("Troconis");
+
+        model.addAttribute("usuario", usuario.getNombre());
+
+        model.addAttribute("apellido", usuario.getApellido());
+
+        return "perfil";
+
+    }
+
 }
